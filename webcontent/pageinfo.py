188963309b52845edd9889715768ff0332fc4618
Austin Pratt <austinjpratt@yahoo.com>
2024-10-30 16:15:12 -0500

    Update pageinfo.py

--- webcontent/pageinfo.py
+++ webcontent/pageinfo.py
@@ -1,1 +1,22 @@
+import requests
+from sys import argv
 
+page = requests.get(f'{argv[1]}')
+
+print('Here are the headers returned from the page:')
+print(page.headers)
+
+print("", end="\n\n")
+
+print('Here is the HTML returned from the page:')
+print(page.txt)
+
+print("", end="\n\n")
+
+print('Here is the HTTP status code returned from the page:')
+print(page.status_code)
+
+print("", end="\n\n")
+
+print('Here are the cookies returned from the page:')
+print(page.cookies)
